cabca39b041fa52a336359eddd5ffa9862685cf8
Manhng83 <manhng83@gmail.com>
2021-05-19 12:15:50 +0700

    WCF Services

---
+++ WcfServices/Interfaces/ISmsServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebApp.WcfServices
+{
+    [ServiceContract]
+    public interface ISmsServices
+    {
+        [OperationContract]
+        string DoWork(string name);
+    }
+}
